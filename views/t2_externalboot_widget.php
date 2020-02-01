daebaed6358424ecf887a4a2ed9c2c65259b84b5
eholtam <(none)>
2020-01-31 22:28:18 -0600

    Widget adjustments

--- views/t2_externalboot_widget.php
+++ views/t2_externalboot_widget.php
@@ -45,8 +45,8 @@ $(document).on('appUpdate', function(e, lang) {
 
 	// Set URLs. TODO - once filtered update this to deep link
 	var url = appUrl + '/show/listing/security/security'
-	$('#t2_externalboot-On').attr('href', url)
-	$('#t2_externalboot-Off').attr('href', url)
+	$('#t2_externalboot-externalbooton').attr('href', url)
+	$('#t2_externalboot-externalbootoff').attr('href', url)
 
         // Show no clients span
         $('#t2_externalboot-nodata').removeClass('hide');
--- views/t2_secureboot_widget.php
+++ views/t2_secureboot_widget.php
@@ -50,9 +50,9 @@ $(document).on('appUpdate', function(e, lang) {
 
 	// Set URLs. TODO - once filtered update this to deep link
 	var url = appUrl + '/show/listing/security/security'
-	$('#t2_secureboot-Off').attr('href', url)
-	$('#t2_secureboot-Medium').attr('href', url)
-	$('#t2_secureboot-Full').attr('href', url)
+	$('#t2_secureboot-securebootoff').attr('href', url)
+	$('#t2_secureboot-securebootmedium').attr('href', url)
+	$('#t2_secureboot-securebootfull').attr('href', url)
 
         // Show no clients span
         $('#t2_secureboot-nodata').removeClass('hide');
