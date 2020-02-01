d7c0dc7810b1d86c4ae556954f7f5b2c471617ed
eholtam <(none)>
2020-01-31 23:44:20 -0600

    Modernized Security detail widget and added Secureboot/Externalboot data points

--- views/security_detail_widget.php
+++ views/security_detail_widget.php
@@ -1,40 +1,71 @@
 <div class="col-lg-4">
     <h4><i class="fa fa-key fa-fixed"></i> <span data-i18n="security.security"></span></h4>
-    <table>
-        <tr>
-            <th data-i18n="security.gatekeeper"></th><td class="mr-gatekeeper"></td>
-        </tr>
-        <tr>
-            <th data-i18n="security.sip"></th><td class="mr-sip"></td>
-        </tr>
-        <tr>
-            <th data-i18n="security.ssh_groups"></th><td class="mr-ssh_groups"></td>
-        </tr>
-        <tr>
-            <th data-i18n="security.ssh_users"></th><td class="mr-ssh_users"></td>
-        </tr>
-        <tr>
-            <th data-i18n="security.ard_users"></th><td class="mr-ard_users"></td>
-        </tr>
-        <tr>
-            <th data-i18n="security.ard_groups"></th><td class="mr-ard_groups"></td>
-        </tr>
-        <tr>
-            <th data-i18n="security.firmwarepw"></th><td class="mr-firmwarepw"></td>
-        </tr>
-        <tr>
-            <th data-i18n="security.firewall_state"></th><td class="mr-firewall_state"></td>
-        </tr>
-        <tr>
-            <th data-i18n="security.skel.kext-loading"></th><td class="mr-skel_state"></td>
-        </tr>
-    </table>
+    <table id="security-data" class="table"></table>
 </div>
 
 <script>
-
-$(document).on('appReady', function(e, lang) {
-
+$(document).on('appReady', function(){
+	// Get security data
+	$.getJSON( appUrl + '/module/security/get_data/' + serialNumber, function( data ) {
+		$.each(data, function(index, item){
+            $('#security-data')
+                .append($('<tr>')
+                    .append($('<th>')
+                        .text(i18n.t('security.gatekeeper')))
+                    .append($('<td>')
+                        .text(item.gatekeeper)))
+                .append($('<tr>')
+                    .append($('<th>')
+                        .text(i18n.t('security.sip')))
+                    .append($('<td>')
+                        .text(item.sip)))
+                .append($('<tr>')
+                    .append($('<th>')
+                        .text(i18n.t('security.ssh_groups')))
+                    .append($('<td>')
+                        .text(item.ssh_groups)))
+                .append($('<tr>')
+                    .append($('<th>')
+                        .text(i18n.t('security.ssh_users')))
+                    .append($('<td>')
+                        .text(item.ssh_users)))
+                .append($('<tr>')
+                    .append($('<th>')
+                        .text(i18n.t('security.ard_users')))
+                    .append($('<td>')
+                        .text(item.ard_users)))
+                .append($('<tr>')
+                    .append($('<th>')
+                        .text(i18n.t('security.ard_groups')))
+                    .append($('<td>')
+                        .text(item.ard_groups)))
+                .append($('<tr>')
+                    .append($('<th>')
+                        .text(i18n.t('security.firmwarepw')))
+                    .append($('<td>')
+                        .text(item.firmwarepw)))
+                .append($('<tr>')
+                    .append($('<th>')
+                        .text(i18n.t('security.firewall_state')))
+                    .append($('<td>')
+                        .text(item.firewall_state)))
+                .append($('<tr>')
+                    .append($('<th>')
+                        .text(i18n.t('security.skel.kext-loading')))
+                    .append($('<td>')
+                        .text(item.skel_state)))
+                .append($('<tr>')
+                    .append($('<th>')
+                        .text(i18n.t('security.t2_secureboot')))
+                    .append($('<td>')
+                        .text(item.t2_secureboot)))
+                .append($('<tr>')
+                    .append($('<th>')
+                        .text(i18n.t('security.t2_externalboot')))
+                    .append($('<td>')
+                        .text(item.t2_externalboot)));
+		});
+    });
 });
-
 </script>
+
